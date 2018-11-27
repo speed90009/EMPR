8ce844e26e49219da6d9c2d173ee74302da031d4
speed90009 <alexander.reed@bourne-grammar.sch.lincs.uk>
2018-11-27 15:24:23 +0000

    MP3 Task 2 Complete

--- MP3.c
+++ MP3.c
@@ -7,11 +7,19 @@
 #include "lpc17xx_nvic.h"
 #include "lpc17xx_gpio.h"
 #include "lpc17xx_adc.h"
+#include "lpc17xx_pwm.h"
+#include "lpc17xx_dac.h"
 #include "math.h"
+#define PI 3.14159265
 
 uint32_t buffer;
-char output[16];
+char data[16];
 DAC_CONVERTER_CFG_Type DACCfg;
+PWM_TIMERCFG_Type PWMCfg;
+PWM_CAPTURECFG_Type PWMCAPCfg;
+int count = 0;
+int counter = 0;
+double val, angle;
 
 void serial_init(void)
 {
@@ -31,6 +39,15 @@ void serial_init(void)
 	PinCfg.Funcnum = 1;
 	PinCfg.Pinnum = 23;
 	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Funcnum = 2;
+	PinCfg.Pinnum = 26;
+	PINSEL_ConfigPin(&PinCfg);
+
+	PinCfg.Funcnum = 1;
+	PinCfg.Portnum = 2;
+	PinCfg.Pinnum = 0;
+	PINSEL_ConfigPin(&PinCfg);
 	
 	UART_ConfigStructInit(&UARTConfigStruct);
 	UART_FIFOConfigStructInit(&UARTFIFOConfigStruct);
@@ -41,7 +58,7 @@ void serial_init(void)
 
 int write_usb_serial_blocking(char *buf,int length)
 {
-	return(UART_Send((LPC_UART_TypeDef *)LPC_UART0,(uint8_t *)bruf,length, BLOCKING));
+	return(UART_Send((LPC_UART_TypeDef *)LPC_UART0,(uint8_t *)buf,length, BLOCKING));
 }
 
 
@@ -56,32 +73,51 @@ void ADC(){
 }
 
 void DAC(){
-	
+	sprintf(data, "TEST TEST no\n\r");
+	write_usb_serial_blocking(data, 16);
 	DAC_Init(LPC_DAC);
-	DAC_ConfigDAConverterControl(LPC_DAC, DACCfg);
+	DAC_SetBias(LPC_DAC, 0);
+	/*	
+	PWM_ConfigStructInit(PWM_MODE_TIMER, &PWMCfg);
+	PWM_Init(LPC_PWM1, PWM_MODE_TIMER, &PWMCfg);
+	PWM_Cmd(LPC_PWM1, ENABLE);
+	PWM_ChannelConfig(LPC_PWM1, 2, PWM_CHANNEL_SINGLE_EDGE);
+	PWM_ConfigCapture(LPC_PWM1, &PWMCAPCfg);
+	//
 	DACCfg.CNT_ENA = 1;
 	DACCfg.DBLBUF_ENA = 1;
-	DACCFG.DMA_ENA = 1;
-	DAC_SetDMATimeout(LPC_DAC, 1000);
+	DACCfg.DMA_ENA = 1;
+	DAC_ConfigDAConverterControl(LPC_DAC, &DACCfg);
+	DAC_SetDMATimeOut(LPC_DAC, 100);
+	*/
 	while(count!=100){count+=1;}
 	
 }
 
-void mp3_task2(int amp, double frq){
-	if (counter / 100 >= 10) {
-		counter -= 1000;
-		DAC(		
+void mp3_task2(void){
+	while(1){
+		counter += 1;
+		if (counter / 100 >= 2) {
+			angle += 1;
+			counter -= 1000;
+			val = PI / 180;
+			DAC_UpdateValue(LPC_DAC, sin(angle*val)*512+512);
+			sprintf(data, "%f\n\r", sin(angle*val)*512+512);
+			write_usb_serial_blocking(data, 16);
+		}
 	}
 }
 
+void DMA_IRQHandler(void){
+	sprintf(data, "TEST TEST Lo\n\r");
+	write_usb_serial_blocking(data, 16);
+}
 
-void main(void){
-	ADC();
+void Task1(void){
 	uint16_t test;
 	int count = 1;
 	char data[50];
 	while(count!=100){count+=1;}
-	serial_init();
 	int i = 0;
 	while(i < 200)
 	{
@@ -92,6 +128,13 @@ void main(void){
 		write_usb_serial_blocking(data, 16);
 		write_usb_serial_blocking("\n\r", 4);
 	}
+}
+
+void main(void){
+	serial_init();
+	DAC();
+	ADC();
+	mp3_task2();
 }
 
 
